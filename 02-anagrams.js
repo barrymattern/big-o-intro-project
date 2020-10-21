e79f379a33060bf2237fb5e7f99f85e127dc0795
Thanh Nguyen <thanhnguyenttn@gmail.com>
2020-10-21 18:33:20 -0400

    thirdAnagram() in progress

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -17,21 +17,48 @@
 // console.log(firstAnagram("gizmo", "sally"));    // => false
 // console.log(firstAnagram("elvis", "lives"));    // => true
 
-
+// T = O(n)
 function secondAnagram(str1, str2) {
-  let sortedStr1 = str1.split("").sort().join("");
-  let sortedStr2 = str2.split("").sort().join("");
+  let sortedStr1 = str1.split("").sort().join(""); // O(n)
+  let sortedStr2 = str2.split("").sort().join(""); // O(n)
 
-  return sortedStr1 === sortedStr2 ? true : false;
+  return sortedStr1 === sortedStr2 ? true : false; //O(1)
 }
 
-console.log(secondAnagram("gizmo", "sally"));    // => false
-console.log(secondAnagram("elvis", "lives"));    // => true
+// console.log(secondAnagram("gizmo", "sally"));    // => false
+// console.log(secondAnagram("elvis", "lives"));    // => true
 
-// function thirdAnagram(str1, str2) {
-//   // Code goes here ....
-// }
+function thirdAnagram(str1, str2) {
+  // Code goes here ....
+  let obj1 = {};
+  let obj2 = {};
 
+  for (let i = 0; i < str1.length; i++){
+    //str1[0], we want letter to be key, and index to be value;
+    let char = str1[i];
+    let char2 = str2[i]
+    if (obj1[char] !== undefined) {
+      obj1[char] += 1;
+    } else {
+      obj1[char] = 1;
+    }
+
+    if(obj2[char2] !== undefined){
+      obj2[char2] += 1;
+    } else {
+      obj2[char2] = 1;
+    }
+  }
+  // console.log("obj1", obj1);
+  // console.log("obj2", obj2);
+
+  return obj1 === obj2;
+
+
+}
+
+console.log(thirdAnagram("gizmo", "sally"));    // => false
+console.log(thirdAnagram("elvis", "lives"));    // => true
 
 // function fourthAnagram(str1, str2) {
 //   // Code goes here ....
