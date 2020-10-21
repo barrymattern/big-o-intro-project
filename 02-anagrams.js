b116c3b44911ef4be48f191a38df30b7c61a4732
Thanh Nguyen <thanhnguyenttn@gmail.com>
2020-10-21 17:35:53 -0400

    firstAnagram() completed, 02-anagram.js

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -1,19 +1,38 @@
 function firstAnagram(str1, str2) {
   // Code goes here ....
+  // iterate through first string = str1;
+  //for each index, see if it exists in str2, using indexOf;
+  // if true, use splice();
+  // return true if str2 is empty after loop of str1 ends.
+
+  for (let i = 0; i < str1.length; i++){
+    let char = str1[i];
+    let char2 = str2.indexOf(char);
+    if (char2 !== -1) {
+      let strBeginning = str2.slice(0, char2);
+      let strEnd = str2.slice(char2 + 1);
+      str2 = strBeginning + strEnd;
+    }
+    if (str2 === "") {
+      return true;
+    }
+  }
+  return false;
 }
+console.log(firstAnagram("gizmo", "sally"));    // => false
+console.log(firstAnagram("elvis", "lives"));    // => true
 
 
-
-function secondAnagram(str1, str2) {
-  // Code goes here ....
-}
-
-
-function thirdAnagram(str1, str2) {
-  // Code goes here ....
-}
+// function secondAnagram(str1, str2) {
+//   // Code goes here ....
+// }
 
 
-function fourthAnagram(str1, str2) {
-  // Code goes here ....
-}
+// function thirdAnagram(str1, str2) {
+//   // Code goes here ....
+// }
+
+
+// function fourthAnagram(str1, str2) {
+//   // Code goes here ....
+// }
