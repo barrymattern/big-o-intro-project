89022ffc4aa71f6e3a946854bfa40fec947f0000
Barry Mattern <bdmattern@gmail.com>
2020-10-21 16:10:30 -0600

    Solve secondAnagram 02-anagrams.js

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -1,32 +1,32 @@
-function firstAnagram(str1, str2) {
-  // Code goes here ....
-  // iterate through first string = str1;
-  //for each index, see if it exists in str2, using indexOf;
-  // if true, use splice();
-  // return true if str2 is empty after loop of str1 ends.
-
-  for (let i = 0; i < str1.length; i++){
-    let char = str1[i];
-    let char2 = str2.indexOf(char);
-    if (char2 !== -1) {
-      let strBeginning = str2.slice(0, char2);
-      let strEnd = str2.slice(char2 + 1);
-      str2 = strBeginning + strEnd;
-    }
-    if (str2 === "") {
-      return true;
-    }
-  }
-  return false;
-}
-console.log(firstAnagram("gizmo", "sally"));    // => false
-console.log(firstAnagram("elvis", "lives"));    // => true
-
-
-// function secondAnagram(str1, str2) {
-//   // Code goes here ....
+// O(n^2)
+// function firstAnagram(str1, str2) {
+//   for (let i = 0; i < str1.length; i++){        //   O(n)
+//     let char = str1[i];                         //   O(1)
+//     let char2 = str2.indexOf(char);             //   O(n)
+//     if (char2 !== -1) {                         //   O(1)
+//       let strBeginning = str2.slice(0, char2);  //   O(n)
+//       let strEnd = str2.slice(char2 + 1);
+//       str2 = strBeginning + strEnd;
+//     }
+//     if (str2 === "") {
+//       return true;
+//     }
+//   }
+//   return false;
 // }
+// console.log(firstAnagram("gizmo", "sally"));    // => false
+// console.log(firstAnagram("elvis", "lives"));    // => true
 
+
+function secondAnagram(str1, str2) {
+  let sortedStr1 = str1.split("").sort().join("");
+  let sortedStr2 = str2.split("").sort().join("");
+
+  return sortedStr1 === sortedStr2 ? true : false;
+}
+
+console.log(secondAnagram("gizmo", "sally"));    // => false
+console.log(secondAnagram("elvis", "lives"));    // => true
 
 // function thirdAnagram(str1, str2) {
 //   // Code goes here ....
