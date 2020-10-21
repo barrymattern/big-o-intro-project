803b6cf9fe678d3665f26aa963bda97142c818c0
Thanh Nguyen <thanhnguyenttn@gmail.com>
2020-10-21 19:25:09 -0400

    okayTwoSum1() in progress

--- 03-two-sum.js
+++ 03-two-sum.js
@@ -12,13 +12,31 @@ function badTwoSum(arr, targetSum) {
 }
 
 const arr = [0, 1, 5, 7];
-console.log(badTwoSum(arr, 6)); // => should be true
-console.log(badTwoSum(arr, 10)); // => should be false
+// console.log(badTwoSum(arr, 6)); // => should be true
+// console.log(badTwoSum(arr, 10)); // => should be false
 
 
 function okayTwoSum1(arr, targetSum) {
   // Code goes here ...
+  // let sorted = arr.sort(); // O(nlogn)
+  let sumArray = [];
+
+  for (let i = 0; i < arr.length/2; i++){
+    let num1 = arr[i];
+    let num2Idx = arr.length - 1;
+    let num2 = arr[num2Idx];
+
+    console.log(num1, num2)
+    if (num1 + num2 > targetSum) {
+      num2Idx--;
+      num2 = arr[num2Idx];
+    } else if (num1 + num2 < targetSum) {
+      num1 = arr[i + 1];
+    }
+  }
 }
+console.log(okayTwoSum1(arr, 6)); // => should be true
+// console.log(okayTwoSum1(arr, 10)); // => should be false
 
 
 function okayTwoSum2(arr, targetSum) {
